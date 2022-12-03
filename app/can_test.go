8a9cdf20d2af5f15026055b1360d0bb7839513b1
Pere <perepicomuntaner@gmail.com>
2022-12-03 09:39:42 +0100

    Add boilerplate code for a test suite

--- app/can_test.go
+++ app/can_test.go
@@ -3,9 +3,28 @@ package app
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
+type TestSuite struct {
+	suite.Suite
+}
+
+func (suite *TestSuite) TestPlay() {
+	tests := []struct {
+		input  string
+		output int
+	}{}
+
+	for _, tt := range tests {
+
+		score, err := BeingTested(tt.input)
+
+		suite.NoError(err)
+		suite.Equal(tt.output, score)
+	}
+}
+
 func TestShouldTest(t *testing.T) {
-	assert.True(t, true, "True is true!")
+	suite.Run(t, new(TestSuite))
 }
